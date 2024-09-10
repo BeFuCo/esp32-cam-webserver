17b8094453fb8086b1eca9905a8a57f39822b43b
Bernd Furnier <bernd.furnier@fu-net.de>
2024-09-10 15:20:44 +0200

    main.cpp: WIFI connection with display of ESP32CAM IP

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
-#include <Arduino.h>
+// code snippets taken from 
+// https://raydiy.de/esp32-cam-platformio-ueberwachungskamera-mit-live-video-stream-per-webserver/
+// René Aye: Thank's for your great tutorial
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Arduino.h>
+#include <WiFi.h>
+
+// Replace with your network credentials
+const char* ssid = "gateway03";
+const char* password = "65505213651803934831";
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+
+  // Connect to Wi-Fi
+  Serial.print("Connecting to WiFi ...");
+
+ WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+
+  // Print ESP Local IP Address
+  Serial.println(" connected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+void loop() {}
